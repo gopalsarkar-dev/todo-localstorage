e7c4b3fa934bd5168dd6d52f0253d52b8e566167
Gopal Sarkar <gopalsarkar.dev@gmail.com>
2025-07-15 20:01:16 +0530

    Mobile menu📱update

--- src/components/Headers/TopBar.tsx
+++ src/components/Headers/TopBar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import DarkToggleBtn from "./DarkToggleBtn";
 import { usePathname } from "next/navigation";
+import MobileMenu from "../MobileMenu";
 
 const TopBar = () => {
   const pathname = usePathname();
@@ -12,22 +13,25 @@ const TopBar = () => {
         <section className="container mx-auto flex max-w-5xl items-center justify-between px-6 py-4">
           <div className="text-lg font-bold">Todo || </div>
           <div className="flex items-center justify-center gap-3">
-            <Link href="/" className={pathname === "/" ? "font-light" : ""}>
-              Todos
-            </Link>
-            <Link
-              href="/complete"
-              className={pathname === "/complete" ? "font-light" : ""}
-            >
-              Complete
-            </Link>
-            <Link
-              href="/incomplete"
-              className={pathname === "/incomplete" ? "font-light" : ""}
-            >
-              InComplet
-            </Link>
+            <div className="hidden space-x-3 sm:block">
+              <Link href="/" className={pathname === "/" ? "font-light" : ""}>
+                Todos
+              </Link>
+              <Link
+                href="/complete"
+                className={pathname === "/complete" ? "font-light" : ""}
+              >
+                Complete
+              </Link>
+              <Link
+                href="/incomplete"
+                className={pathname === "/incomplete" ? "font-light" : ""}
+              >
+                InComplet
+              </Link>
+            </div>
             <DarkToggleBtn />
+            <MobileMenu />
           </div>
         </section>
       </nav>
